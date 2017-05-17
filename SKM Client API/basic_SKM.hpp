de8514fe18b6ab7adb1f62484f7360dd31caf97d
Martin Svedin <svedi@kth.se>
2017-05-17 10:36:40 +0000

    Adding version of activate which throws exception

--- SKM Client API/basic_SKM.hpp
+++ SKM Client API/basic_SKM.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cstring>
 #include <string>
+#include <sstream>
 #include <unordered_map>
 
 #include "optional.hpp"
@@ -10,10 +12,13 @@
 #include "RawLicenseKey.hpp"
 #include "LicenseKey.hpp"
 #include "LicenseKeyChecker.hpp"
+#include "api.hpp"
 
 namespace serialkeymanager_com {
 
 using namespace ArduinoJson;
+
+class ActivateError;
 
 // Helper class to work with the FieldsToReturn parameter when
 // making a Activate request to the Web API.
@@ -69,6 +74,35 @@ struct FieldsToReturn {
   }
 };
 
+template<typename RequestHandler>
+std::string
+make_activate_request
+  ( RequestHandler & request_handler
+  , std::string const& token
+  , std::string const& product_id
+  , std::string const& key
+  , std::string const& machine_code
+  , int fields_to_return = 0
+  )
+{
+  std::unordered_map<std::string,std::string> args;
+  args["token"] = token;
+  args["ProductId"] = product_id;
+  args["Key"] = key;
+  args["Sign"] = "true";
+  args["MachineCode"] = machine_code;
+  // Fix since to_string is not available everywhere
+  //args["FieldsToReturn"] = std::to_string(fields_to_return);
+  std::ostringstream stm; stm << fields_to_return;
+  args["FieldsToReturn"] = stm.str();
+  args["SignMethod"] = "1";
+  args["v"] = "1";
+
+  std::string url = request_handler.build_url("Activate", args);
+
+  return request_handler.make_request(url);
+}
+
 // Function for handling a response to an Activate request from
 // the SKM Web API
 template<typename SignatureVerifier>
@@ -78,24 +112,56 @@ handle_activate
   , std::string const& response
   )
 {
+  try {
+    return make_optional(
+	     handle_activate_exn( signature_verifier
+		                , response)
+	   );
+  } catch (ActivateError const& e) {
+    return nullopt;
+  }
+}
+
+template<typename SignatureVerifier>
+RawLicenseKey
+handle_activate_exn
+  ( experimental_v1 experimental
+  , SignatureVerifier const& signature_verifier
+  , std::string const& response
+  )
+{
   DynamicJsonBuffer jsonBuffer;
   JsonObject & j = jsonBuffer.parseObject(response);
 
-  if (!j.success()) { return nullopt; }
+  if (!j.success()) { throw ActivateError(NULL); }
+
+  if (!j["result"].is<int>() || j["result"].as<int>() != 0) {
+    if (!j["message"].is<const char*>() || j["message"].as<char const*>() == NULL) {
+      throw ActivateError(NULL);
+    }
+
+    throw ActivateError(j["message"].as<char const*>());
+  }
 
   if (!j["licenseKey"].is<char const*>() || j["licenseKey"].as<char const*>() == NULL) {
-    return nullopt;
+    throw ActivateError(NULL);
   }
 
   if (!j["signature"].is<char const*>() || j["signature"].as<char const*>() == NULL) {
-    return nullopt;
+    throw ActivateError(NULL);
   }
 
-  return RawLicenseKey::make(
+  optional<RawLicenseKey> raw = RawLicenseKey::make(
              signature_verifier
            , j["licenseKey"].as<char const*>()
            , j["signature"].as<char const*>()
 	   );
+
+  if (raw) {
+    return *raw;
+  } else {
+    throw ActivateError(NULL);
+  }
 }
 
 // Function for handling a response to an Deactivate request from
@@ -112,11 +178,11 @@ handle_deactivate
 
   if (!j.success()) { return false; }
 
-  if (!j["Result"].is<int>()) {
+  if (!j["result"].is<int>()) {
     return false;
   }
 
-  return j["Result"].as<int>() == 0;
+  return j["result"].as<int>() == 0;
 }
 
 // This class makes it possible to interact with the SKM Web API. Among the
@@ -153,21 +219,52 @@ public:
     , int fields_to_return = 0
     )
   {
-    std::unordered_map<std::string,std::string> args;
-    args["token"] = token;
-    args["ProductId"] = product_id;
-    args["Key"] = key;
-    args["Sign"] = "true";
-    args["MachineCode"] = machine_code;
-    args["FieldsToReturn"] = std::to_string(fields_to_return);
-    args["SignMethod"] = "1";
-    args["v"] = "1";
-
-    std::string url = request_handler.build_url("Activate", args);
-
-    std::string response = request_handler.make_request(url);
+    std::string response =
+      make_activate_request
+        ( this->request_handler
+        , token
+        , product_id
+        , key
+        , machine_code
+        , fields_to_return
+      );
 
     return handle_activate(this->signature_verifier, response);
+  }
+
+  // Make an Activate request to the SKM Web API
+  //
+  // Arguments:
+  //   token - acces token to use
+  //   product_id - the product id
+  //   key - the serial key string, e.g. ABCDE-EFGHI-JKLMO-PQRST
+  //   machine_code - the machine code, i.e. a string that identifies a device
+  //                  for activation.
+  //
+  // Returns:
+  //   An optional with a RawLicenseKey, if the request is successful this always
+  //   contains a value. If the request is unsuecessful an ActivateError is thrown.
+  RawLicenseKey
+  activate_exn
+    ( experimental_v1 experimental
+    , std::string token
+    , std::string product_id
+    , std::string key
+    , std::string machine_code
+    , int fields_to_return = 0
+    )
+  {
+    std::string response =
+      make_activate_request
+        ( this->request_handler
+        , token
+        , product_id
+        , key
+        , machine_code
+        , fields_to_return
+      );
+
+    return handle_activate_exn(experimental, this->signature_verifier, response);
   }
 
   // Make an Deactivate request to the SKM Web API
@@ -205,6 +302,88 @@ public:
  
   SignatureVerifier signature_verifier;
   RequestHandler request_handler;
+};
+
+class ActivateError : public std::exception {
+private:
+  int reason;
+
+public:
+  static const int UNKNOWN_SERVER_REPLY      = 0;
+  static const int INVALID_ACCESS_TOKEN      = 1;
+  static const int ACCESS_DENIED             = 2;
+  static const int INCORRECT_INPUT_PARAMETER = 3;
+  static const int PRODUCT_NOT_FOUND         = 4;
+  static const int KEY_NOT_FOUND             = 5;
+  static const int KEY_BLOCKED               = 6;
+  static const int DEVICE_LIMIT_REACHED      = 7;
+
+  ActivateError(const char *server_response)
+  {
+    reason = UNKNOWN_SERVER_REPLY;
+    if (server_response == NULL) { return; }
+
+    if (0 == std::strcmp(server_response, "Unable to authenticate.")) {
+      reason = INVALID_ACCESS_TOKEN;
+    }
+
+    if (0 == std::strcmp(server_response, "Access denied.")) {
+      reason = ACCESS_DENIED;
+    }
+
+    if (0 == std::strcmp(server_response, "The input parameters were incorrect.")) {
+      reason = INCORRECT_INPUT_PARAMETER;
+    }
+
+    if (0 == std::strcmp(server_response, "Could not find the product.")) {
+      reason = PRODUCT_NOT_FOUND;
+    }
+
+    if (0 == std::strcmp(server_response, "Could not find the key.")) {
+      reason = KEY_NOT_FOUND;
+    }
+
+    if (0 == std::strcmp(server_response, "The key is blocked and cannot be accessed.")) {
+      reason = KEY_BLOCKED;
+    }
+
+    if (0 == std::strcmp(server_response, "Cannot activate the new device as the limit has been reached.")) {
+      reason = DEVICE_LIMIT_REACHED;
+    }
+  }
+
+  int get_reason() { return reason; }
+
+  const char * what() {
+    switch (reason) {
+    case INVALID_ACCESS_TOKEN:
+    return "Invalid access token.";
+
+    case UNKNOWN_SERVER_REPLY:
+    return "Recieved unknown reply from the server.";
+
+    case ACCESS_DENIED:
+    return "Access denied.";
+
+    case INCORRECT_INPUT_PARAMETER:
+    return "The input parameters were incorrect.";
+
+    case PRODUCT_NOT_FOUND:
+    return "Could not find the product." ;
+
+    case KEY_NOT_FOUND:
+    return "Could not find the key.";
+
+    case KEY_BLOCKED:
+    return "The key is blocked and cannot be accessed.";
+
+    case DEVICE_LIMIT_REACHED:
+    return "Cannot activate the new device as the limit has been reached.";
+
+    default:
+    return "Unknown error.";
+    }
+  }
 };
 
 } // namespace serialkeymanager_com
