5b3adb4b4b1826b8a0ab2dec3679331bbd3a229c
Martin Svedin <svedi@kth.se>
2017-07-01 14:53:28 +0000

    SQUASH

--- SKM Client API/Error.hpp
+++ SKM Client API/Error.hpp
@@ -8,49 +8,62 @@ class Error {
  *   - 
  */
 private:
-  size_t reason_;
+  // Should add one more, we want support for tracking which method call
+  // by the user triggered the error, and then locating the error.
+  // For location we need basically to track three things, first
+  // which function/method caused the error, then some location inside
+  // that function/method and finally possibility to carry forward errors
+  // from other libraries.
+  int    call_;
+  int    source_;
+  int    reason_;
+  size_t extra_;
 public:
-  Error(): reason_(NO_ERROR) { }
+  Error(): call_(0), source_(NO_ERROR), reason_(0), extra_(0) { }
   Error(Error const& e) = delete;
   Error & operator=(Error const& e) = delete;
 
-  static constexpr size_t NO_ERROR = 0;
+  static constexpr int NO_ERROR = 0;
 
-  explicit operator bool() const { return reason_ != NO_ERROR; }
+  explicit operator bool() const { return source_ != NO_ERROR; }
 
-  size_t get_reason() const noexcept { return reason_; }
+  int get_source() const noexcept { return source_; }
+  int get_reason() const noexcept { return reason_; }
+  size_t get_extra() const noexcept { return extra_; }
 
-  void reset() { reason_ = NO_ERROR; }
+  void reset() { source_ = NO_ERROR; reason_ = 0; extra_ = 0; }
 
-  void set(size_t reason) { reason_ = reason; }
+  void set(int source) { source_ = source; }
+  void set(int source, int reason) { source_ = source; reason_ = reason; }
+  void set(int source, int reason, size_t extra) { source_ = source; reason_ = reason; extra_ = extra; }
 
-  static size_t constexpr MAKE_REQUEST_CURL_NULL = 1;
-  static size_t constexpr MAKE_REQUEST_SETOPT = 2;
-  static size_t constexpr MAKE_REQUEST_PERFORM = 3;
-  static size_t constexpr VERIFY_RSA_NULL = 4;
-  static size_t constexpr VERIFY_RSA_CTX_CREATE = 5;
-  static size_t constexpr VERIFY_RSA_PKEY_NEW = 6;
-  static size_t constexpr VERIFY_RSA_PKEY_SET = 7;
-  static size_t constexpr VERIFY_EVP_INIT = 8;
-  static size_t constexpr VERIFY_EVP_UPDATE = 9;
-  static size_t constexpr VERIFY_EVP_FINAL = 10;
-  static size_t constexpr SET_MODULUS_RSA_NULL = 11;
-  static size_t constexpr SET_MODULUS_B64_DECODE_FAILED = 12;
-  static size_t constexpr SET_EXPONENT_RSA_NULL = 13;
-  static size_t constexpr SET_EXPONENT_B64_DECODE_FAILED = 14;
-  static size_t constexpr VERIFY_MESSAGE_RSA_NULL = 15;
-  static size_t constexpr VERIFY_MESSAGE_B64_DECODE_FAILED = 16;
-  static size_t constexpr BUILD_URL_CURL_NULL = 17;
-  static size_t constexpr BUILD_URL_ESCAPE = 18;
-  static size_t constexpr RAWLICENSEKEY_MAKE_B64DECODE = 19;
-  static size_t constexpr HANDLE_ACTIVATE_JSON_PARSE_FAILED = 20;
-  static size_t constexpr HANDLE_ACTIVATE_FAIL_MESSAGE_MISSING = 21;
-  static size_t constexpr HANDLE_ACTIVATE_FIXME = 200000;
-  static size_t constexpr HANDLE_ACTIVATE_LICENSE_MISSING = 22;
-  static size_t constexpr HANDLE_ACTIVATE_SIGNATURE_MISSING = 23;
-  static size_t constexpr RAWLICENSEKEY_MAKE_B64_DECODE_FAILED = 24;
-  static size_t constexpr LICENSEKEY_MAKE_JSON_PARSE_FAILED = 25;
-  static size_t constexpr LICENSEKEY_MAKE_MANDATORY_MISSING = 26;
+  static int constexpr MAKE_REQUEST_CURL_NULL = 1;
+  static int constexpr MAKE_REQUEST_SETOPT = 2;
+  static int constexpr MAKE_REQUEST_PERFORM = 3;
+  static int constexpr VERIFY_RSA_NULL = 4;
+  static int constexpr VERIFY_RSA_CTX_CREATE = 5;
+  static int constexpr VERIFY_RSA_PKEY_NEW = 6;
+  static int constexpr VERIFY_RSA_PKEY_SET = 7;
+  static int constexpr VERIFY_EVP_INIT = 8;
+  static int constexpr VERIFY_EVP_UPDATE = 9;
+  static int constexpr VERIFY_EVP_FINAL = 10;
+  static int constexpr SET_MODULUS_RSA_NULL = 11;
+  static int constexpr SET_MODULUS_B64_DECODE_FAILED = 12;
+  static int constexpr SET_EXPONENT_RSA_NULL = 13;
+  static int constexpr SET_EXPONENT_B64_DECODE_FAILED = 14;
+  static int constexpr VERIFY_MESSAGE_RSA_NULL = 15;
+  static int constexpr VERIFY_MESSAGE_B64_DECODE_FAILED = 16;
+  static int constexpr BUILD_URL_CURL_NULL = 17;
+  static int constexpr BUILD_URL_ESCAPE = 18;
+  static int constexpr RAWLICENSEKEY_MAKE_B64DECODE = 19;
+  static int constexpr HANDLE_ACTIVATE_JSON_PARSE_FAILED = 20;
+  static int constexpr HANDLE_ACTIVATE_FAIL_MESSAGE_MISSING = 21;
+  static int constexpr HANDLE_ACTIVATE_FIXME = 200000;
+  static int constexpr HANDLE_ACTIVATE_LICENSE_MISSING = 22;
+  static int constexpr HANDLE_ACTIVATE_SIGNATURE_MISSING = 23;
+  static int constexpr RAWLICENSEKEY_MAKE_B64_DECODE_FAILED = 24;
+  static int constexpr LICENSEKEY_MAKE_JSON_PARSE_FAILED = 25;
+  static int constexpr LICENSEKEY_MAKE_MANDATORY_MISSING = 26;
 //  static size_t constexpr 
 };
 
