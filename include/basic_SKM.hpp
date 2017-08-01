ef34491161d167f8e132abf0f0b68270ef6094a1
Martin Svedin <svedi@kth.se>
2017-08-01 08:30:38 +0000

    Move definitions out of basic_SKM class definition

--- include/basic_SKM.hpp
+++ include/basic_SKM.hpp
@@ -108,27 +108,6 @@ handle_activate_exn
   }
 }
 
-// Function for handling a response to an Deactivate request from
-// the SKM Web API
-template<typename SignatureVerifier>
-bool
-handle_deactivate
-  ( SignatureVerifier const& signature_verifier
-  , std::string const& response
-  )
-{
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject & j = jsonBuffer.parseObject(response);
-
-  if (!j.success()) { return false; }
-
-  if (!j["result"].is<int>()) {
-    return false;
-  }
-
-  return j["result"].as<int>() == 0;
-}
-
 // This class makes it possible to interact with the SKM Web API. Among the
 // various methods available in the Web API the only ones currently supported
 // in the C++ API are Activate and Deactivate.
@@ -161,20 +140,7 @@ public:
     , std::string key
     , std::string machine_code
     , int fields_to_return = 0
-    )
-  {
-    std::string response =
-      make_activate_request
-        ( this->request_handler
-        , token
-        , product_id
-        , key
-        , machine_code
-        , fields_to_return
-      );
-
-    return handle_activate(this->signature_verifier, response);
-  }
+    );
 
   // Make an Activate request to the SKM Web API
   //
@@ -196,56 +162,57 @@ public:
     , std::string key
     , std::string machine_code
     , int fields_to_return = 0
-    )
-  {
-    std::string response =
-      make_activate_request
-        ( this->request_handler
-        , token
-        , product_id
-        , key
-        , machine_code
-        , fields_to_return
-      );
-
-    return handle_activate_exn(experimental, this->signature_verifier, response);
-  }
-
-  // Make an Deactivate request to the SKM Web API
-  //
-  // Arguments:
-  //   token - acces token to use
-  //   product_id - the product id
-  //   key - the serial key string, e.g. ABCDE-EFGHI-JKLMO-PQRST
-  //   machine_code - the machine code, i.e. a string that identifies a device
-  //                  for activation.
-  //
-  // Returns:
-  //   A boolean representing if the request was successful or not.
-  bool
-  deactivate
-    ( std::string token
-    , std::string product_id
-    , std::string key
-    , std::string machine_code
-    )
-  {
-    std::unordered_map<std::string,std::string> args;
-    args["token"] = token;
-    args["ProductId"] = product_id;
-    args["Key"] = key;
-    args["MachineCode"] = machine_code;
-    args["v"] = "1";
-
-    std::string url = request_handler.build_url("Deactivate", args);
-
-    std::string response = request_handler.make_request(url);
-
-    return handle_deactivate(this->signature_verifier, response);
-  }
+    );
 
   SignatureVerifier signature_verifier;
   RequestHandler request_handler;
 };
+
+template<typename RequestHandler, typename SignatureVerifier>
+optional<RawLicenseKey>
+basic_SKM<RequestHandler, SignatureVerifier>::activate
+  ( std::string token
+  , std::string product_id
+  , std::string key
+  , std::string machine_code
+  , int fields_to_return
+  )
+{
+  std::string response =
+    make_activate_request
+      ( this->request_handler
+      , token
+      , product_id
+      , key
+      , machine_code
+      , fields_to_return
+    );
+
+  return handle_activate(this->signature_verifier, response);
+}
+
+template<typename RequestHandler, typename SignatureVerifier>
+RawLicenseKey
+basic_SKM<RequestHandler, SignatureVerifier>::activate_exn
+  ( experimental_v1 experimental
+  , std::string token
+  , std::string product_id
+  , std::string key
+  , std::string machine_code
+  , int fields_to_return
+  )
+{
+  std::string response =
+    make_activate_request
+      ( this->request_handler
+      , token
+      , product_id
+      , key
+      , machine_code
+      , fields_to_return
+    );
+
+  return handle_activate_exn(experimental, this->signature_verifier, response);
+}
 
 } // namespace serialkeymanager_com
