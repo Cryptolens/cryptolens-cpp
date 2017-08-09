ea7a20b5b036731ee10234efbbe931f8d366a204
Martin Svedin <svedi@kth.se>
2017-08-09 13:04:08 +0000

    Make members public and unhide inherited methods for Error

--- include/Error.hpp
+++ include/Error.hpp
@@ -5,11 +5,15 @@
 namespace serialkeymanager_com {
 
 class Error : public basic_Error {
-  // TODO: Why is it neccessary to prefix basic_Error here for the compiler to
-  //       find the methods?
-  int get_subsystem() const noexcept { return basic_Error::get_subsystem(api::main()); }
-  int get_reason() const noexcept { return basic_Error::get_reason(api::main()); }
-  size_t get_extra() const noexcept { return basic_Error::get_extra(api::main()); }
+public:
+  // Declarations below hide these methods inherited from the basic_Error class
+  using basic_Error::get_subsystem;
+  using basic_Error::get_reason;
+  using basic_Error::get_extra;
+
+  int get_subsystem() const noexcept { return get_subsystem(api::main()); }
+  int get_reason() const noexcept { return get_reason(api::main()); }
+  size_t get_extra() const noexcept { return get_extra(api::main()); }
 };
 
 } // namespace serialkeymanager_com
