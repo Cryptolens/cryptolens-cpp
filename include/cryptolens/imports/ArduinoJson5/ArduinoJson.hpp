1e3d668260672eb45efeb5b704ef25f6718a32c1
Martin Svedin <martin@cryptolens.io>
2023-01-05 15:15:08 +0000

    Merged into master

--- include/cryptolens/imports/ArduinoJson5/ArduinoJson.hpp
+++
@@ -1,9 +0,0 @@
-#ifdef CRYPTOLENS_SHORT_INCLUDE_PATHS
-
-#include <ArduinoJson.hpp>
-
-#else
-
-#include "../../../../third_party/ArduinoJson5/ArduinoJson.hpp"
-
-#endif
--- include/cryptolens/imports/openssl/bn.h
+++
@@ -1,1 +0,0 @@
-#include <openssl/bn.h>
