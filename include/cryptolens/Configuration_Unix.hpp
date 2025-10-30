17f3a63ccf06d74b9138df4c3f56b6248df59f5e
Martin Svedin <martin@cryptolens.io>
2025-10-30 11:06:48 +0100

    Add SignatureVerifier_OpenSSL3

--- include/cryptolens/Configuration_Unix.hpp
+++ include/cryptolens/Configuration_Unix.hpp
@@ -2,7 +2,13 @@
 
 #include "ResponseParser_ArduinoJson7.hpp"
 #include "RequestHandler_curl.hpp"
+
+#include <openssl/opensslv.h>
+#if OPENSSL_VERSION_NUMBER >= 0x030000000
+#include "SignatureVerifier_OpenSSL3.hpp"
+#else
 #include "SignatureVerifier_OpenSSL.hpp"
+#endif
 
 #include "validators/AndValidator.hpp"
 #include "validators/CorrectKeyValidator.hpp"
@@ -18,8 +24,13 @@ template<typename MachineCodeComputer_>
 struct Configuration_Unix {
   using ResponseParser = ResponseParser_ArduinoJson7;
   using RequestHandler = RequestHandler_curl;
-  using SignatureVerifier = SignatureVerifier_OpenSSL;
   using MachineCodeComputer = MachineCodeComputer_;
+
+#if OPENSSL_VERSION_NUMBER >= 0x030000000
+  using SignatureVerifier = SignatureVerifier_OpenSSL3;
+#else
+  using SignatureVerifier = SignatureVerifier_OpenSSL;
+#endif
 
   template<typename Env>
   using ActivateValidator = AndValidator_<Env, CorrectKeyValidator_<Env>
@@ -38,8 +49,13 @@ template<typename MachineCodeComputer_>
 struct Configuration_Unix_IgnoreExpires {
   using ResponseParser = ResponseParser_ArduinoJson7;
   using RequestHandler = RequestHandler_curl;
-  using SignatureVerifier = SignatureVerifier_OpenSSL;
   using MachineCodeComputer = MachineCodeComputer_;
+
+#if OPENSSL_VERSION_NUMBER >= 0x030000000
+  using SignatureVerifier = SignatureVerifier_OpenSSL3;
+#else
+  using SignatureVerifier = SignatureVerifier_OpenSSL;
+#endif
 
   template<typename Env>
   using ActivateValidator = AndValidator_<Env, CorrectKeyValidator_<Env>
